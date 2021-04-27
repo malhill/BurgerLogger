9abd4650f6c275f84b07103b8933f38bd3f8f977
malhill <74620676+malhill@users.noreply.github.com>
2021-04-27 11:47:43 -0400

    fixed connection.js added db

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,18 @@ const mysql = require("mysql");
 
 let connection; 
 
-let connection = mysql.createConnection({
+let connection;
+if(proccess.env.JAWSDB_URL) {
+    connection = mysql.createConnection(proccess.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 8080,
     user: "root",
     password: "password",
     database: "burger_db"
-});
+    })
+};
 
 connection.connect(function (err) {
     if (err) throw err;
