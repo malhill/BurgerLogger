97fdb278fcc857cafcd2b31a2d0dd529da615dcc
malhill <74620676+malhill@users.noreply.github.com>
2021-04-27 12:05:11 -0400

    made edits to connections agian,corrected typos

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,10 @@ if(process.env.JAWSDB_URL) {
 } else {
     connection = mysql.createConnection({
     host: "localhost",
-    port: 8080,
+    port: 3306,
     user: "root",
     password: "password",
-    database: "burger_db"
+    database: "burgers_db"
     })
 };
 
